e07f48f09fd2ae8e1b8591356ba612d482c54e16
Mahesh Chopade <65208771+mahesh2-lab@users.noreply.github.com>
2024-12-19 03:14:10 +0000

    some chnages complete

--- src/App.jsx
+++ src/App.jsx
@@ -4,29 +4,37 @@ import NavBar from "./component/NavBar/NavBar";
 import Parallax from "./component/Parallax/Parallax";
 import AboutMe from "./component/aboutMe/abooutMe";
 import Contact from "./component/contact/Contact";
+import Cursor from "./component/cursore/CustomCursor";
+import { CursorContextProvider } from "./component/cursore/context/CursorContext";
 import Portfolio from "./component/portfollio/Portfolio";
 
 function App() {
   return (
     <>
-      <div>
-        <section id="Home">
-          <NavBar />
-          <Hero />
-        </section>
-        <section id="About">
-          <Parallax type="services" />
-        </section>
-        <section>
-          <AboutMe />
-        </section>
-        <section id="Projects">
-          <Parallax type="portfolio" />
-        </section>
-        <Portfolio />
-        <section id="Contact">
-          <Contact />
-        </section>
+
+      <div id="app">
+        <CursorContextProvider>
+          {/* <Cursor /> */}
+          <div>
+            <section id="Home">
+              <NavBar />
+              <Hero />
+            </section>
+            <section id="About">
+              <Parallax type="services" />
+            </section>
+            <section>
+              <AboutMe />
+            </section>
+            <section id="Projects">
+              <Parallax type="portfolio" />
+            </section>
+            <Portfolio />
+            <section id="Contact">
+              <Contact />
+            </section>
+          </div>
+        </CursorContextProvider>
       </div>
     </>
   );
--- src/component/NavBar/NavBar.jsx
+++ src/component/NavBar/NavBar.jsx
@@ -22,13 +22,13 @@ const NavBar = () => {
             ></lord-icon>
           </motion.span>
           <div className="social">
-            <a href="https://www.twitter.com/">
-              <img src="twitter.png" alt="Twitter" />
+            <a href="https://wa.me/8975252853" target="_blank">
+              <img src="whatsapp.png" alt="Whatsapp" />
             </a>
-            <a href="https://www.linkedin.com/">
+            <a href="https://www.linkedin.com/in/mahesh-chopade-05309a256/" target="_blank">
               <img src="linkedin.png" alt="LinkedIn" />
             </a>
-            <a href="https://www.instagram.com/">
+            <a href="https://www.instagram.com/mahesh_0_8_8/" target="_blank">
               <img src="instagram.png" alt="Instagram" />
             </a>
           </div>
