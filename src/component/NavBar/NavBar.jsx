28e6c49cd7b4af2270a68b4c0d45cb2e63405c5d
Mahesh <maheshchopade133@gmail.com>
2025-01-05 15:31:42 +0530

    vv

--- src/component/NavBar/NavBar.jsx
+++ src/component/NavBar/NavBar.jsx
@@ -22,7 +22,6 @@ const NavBar = () => {
             ></lord-icon>
           </motion.span>
           <div className="social">
-<<<<<<< HEAD
             <a href="https://wa.me/8975252853">
               <img src="whatsapp.png" alt="Whatsapp" />
             </a>
@@ -30,15 +29,6 @@ const NavBar = () => {
               <img src="linkedin.png" alt="LinkedIn" />
             </a>
             <a href="https://www.instagram.com/mahesh_0_8_8/">
-=======
-            <a href="https://wa.me/8975252853" target="_blank">
-              <img src="whatsapp.png" alt="Whatsapp" />
-            </a>
-            <a href="https://www.linkedin.com/in/mahesh-chopade-05309a256/" target="_blank">
-              <img src="linkedin.png" alt="LinkedIn" />
-            </a>
-            <a href="https://www.instagram.com/mahesh_0_8_8/" target="_blank">
->>>>>>> e07f48f09fd2ae8e1b8591356ba612d482c54e16
               <img src="instagram.png" alt="Instagram" />
             </a>
           </div>
